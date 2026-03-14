e92be0e95a5f0ee8f4056ceafbd37df4f7a2144e
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-03-14 19:09:25 +0530

    Time: 4 ms (81.45%), Space: 46.4 MB (44.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,12 +1,9 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
-            return false;
-        }
         char[] sa = s.toCharArray();
         char[] ta = t.toCharArray();
         Arrays.sort(sa);
         Arrays.sort(ta);
-        return Arrays.equals(sa,ta);
+        return sa.length==ta.length && Arrays.equals(sa,ta);
     }
 }
