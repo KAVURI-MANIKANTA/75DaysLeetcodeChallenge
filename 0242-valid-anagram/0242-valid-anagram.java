ce0b0dc11410c718169db319eb839392d5a43f52
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-03-14 19:06:48 +0530

    Time: 4 ms (81.45%), Space: 46.5 MB (39.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,27 +5,10 @@ class Solution {
         if(sl!=tl){
             return false;
         }
-        HashMap<Character,Integer> shm = new HashMap<>();
-        HashMap<Character,Integer> thm = new HashMap<>();
-        for(int i=0; i<sl; i++){
-            char ch = s.charAt(i);
-            shm.put(ch,shm.getOrDefault(ch,0)+1);
-        }
-        for(int i=0; i<tl; i++){
-            char ch = t.charAt(i);
-            thm.put(ch,thm.getOrDefault(ch,0)+1);
-        }
-        if(shm.size()!=thm.size()){
-            return false;
-        }
-        for(char ch:shm.keySet()){
-            if(!thm.containsKey(ch)){
-                return false;
-            }
-            if(!shm.get(ch).equals(thm.get(ch))){
-                return false;
-            }
-        }
-        return true;
+        char[] sarr = s.toCharArray();
+        char[] tarr = t.toCharArray();
+        Arrays.sort(sarr);
+        Arrays.sort(tarr);
+        return Arrays.equals(sarr,tarr);
     }
 }
