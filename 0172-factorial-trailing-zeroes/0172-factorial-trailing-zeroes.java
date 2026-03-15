e9121d623d7b92539446136df1a59a299a71994e
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-03-15 22:21:24 +0530

    Time: 0 ms (100%), Space: 41.7 MB (99.62%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,7 +1,6 @@
 class Solution {
     public int trailingZeroes(int n) {
-        int count = 0;
-        int sum = 0;
+        int sum=0;
         while(n>0){
             sum = sum + n/5;
             n=n/5;
