9b2e31d3af934d6e701b244cccd35b139a3554a7
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-03-27 22:52:18 +0530

    Time: 1 ms (99.89%), Space: 43.2 MB (67.05%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,18 +1,8 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int[] freq = new int[26];
-        int sl = s.length();
-        for(int i=0; i<sl; i++){
-            freq[s.charAt(i)-'a']++;
-        }
-        int tl = t.length();
-        for(int i=0; i<tl; i++){
-            char ch = t.charAt(i);
-            if(freq[ch-'a']==0){
-                return ch;
-            }
-            freq[ch-'a']--;
-        }
-        return ' ';
+        int result = 0;
+        for(char ch:s.toCharArray()) result = result^ch;
+        for(char ch:t.toCharArray()) result = result^ch;
+        return (char)result;
     }
 }
