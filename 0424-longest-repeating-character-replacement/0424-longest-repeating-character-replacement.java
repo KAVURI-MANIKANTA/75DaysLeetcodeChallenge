00675224ad1a5aa65dab1b2153bac896941a14a5
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-03-28 22:50:42 +0530

    Time: 6 ms (99%), Space: 46 MB (93.66%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,8 +1,6 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int l = 0;
-        int maxFreq = 0;
-        int res = 0;
+        int l = 0, maxFreq = 0, res = 0;
         int[] freq = new int[26];
         for(int r=0; r<s.length(); r++){
             int ch = s.charAt(r)-'A';
