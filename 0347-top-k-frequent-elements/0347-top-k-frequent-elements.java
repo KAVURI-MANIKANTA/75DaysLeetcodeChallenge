3c704e144c03c734b68cae23697d67947177eb48
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-03-29 23:35:28 +0530

    Time: 16 ms (40.2%), Space: 47.5 MB (74.82%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -5,6 +5,19 @@ class Solution {
         for(int i=0; i<n; i++){
             hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->hm.get(b)-hm.get(a));
+        pq.addAll(hm.keySet());
+        int[] res = new int[k];
+        for(int i=0; i<k; i++){
+            res[i] = pq.poll();
+        }
+        return res;
+        /*
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+        }
         List<Integer>[] sorthm = new List[n+1];
         for(int key: hm.keySet()){
             int freq = hm.get(key);
@@ -24,5 +37,6 @@ class Solution {
             }
         }
         return res;
+        */
     }
 }
