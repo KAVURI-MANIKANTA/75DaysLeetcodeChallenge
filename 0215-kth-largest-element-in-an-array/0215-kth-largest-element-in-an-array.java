3d8dc68115b88f1f932ca8283bac67aa5a4b79cd
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-03-29 23:09:50 +0530

    Time: 43 ms (72.76%), Space: 65.6 MB (95.84%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -12,5 +12,49 @@ class Solution {
             }
         }
         return pq.poll();
+
+
+        /*
+        class Solution {
+            public int findKthLargest(int[] nums, int k) {
+                Arrays.sort(nums);
+                return nums[nums.length-k];
+            }
+        }
+        */
+
+        /*
+        class Solution {
+            public int findKthLargest(int[] nums, int k) {
+                PriorityQueue<Integer> pq = new PriorityQueue<>();
+                for(int n:nums){
+                    pq.add(n);
+                    if(pq.size()>k){
+                        pq.poll();
+                    }
+                }
+                return pq.poll();
+            }
+        }
+        */
+
+
+        /*
+        class Solution {
+            public int findKthLargest(int[] nums, int k) {
+                PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+                int n = nums.length;
+                for(int i=0; i<n; i++){
+                    pq.add(nums[i]);
+                }
+                int res = 0;
+                for(int i=0; i<k; i++){
+                    res = pq.poll();
+                }
+                return res;
+            }
+        }
+        */
+
     }
 }
