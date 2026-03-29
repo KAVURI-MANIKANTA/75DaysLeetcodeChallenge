a5ae9d38e7181faa9d547f3118f124db972bf042
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-03-29 23:36:38 +0530

    Time: 11 ms (96.03%), Space: 47.4 MB (86.81%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -5,19 +5,6 @@ class Solution {
         for(int i=0; i<n; i++){
             hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->hm.get(b)-hm.get(a));
-        pq.addAll(hm.keySet());
-        int[] res = new int[k];
-        for(int i=0; i<k; i++){
-            res[i] = pq.poll();
-        }
-        return res;
-        /*
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        int n = nums.length;
-        for(int i=0; i<n; i++){
-            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
-        }
         List<Integer>[] sorthm = new List[n+1];
         for(int key: hm.keySet()){
             int freq = hm.get(key);
@@ -35,6 +22,19 @@ class Solution {
                     if(count==k) return res;
                 }
             }
+        }
+        return res;
+        /*
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->hm.get(b)-hm.get(a));
+        pq.addAll(hm.keySet());
+        int[] res = new int[k];
+        for(int i=0; i<k; i++){
+            res[i] = pq.poll();
         }
         return res;
         */
