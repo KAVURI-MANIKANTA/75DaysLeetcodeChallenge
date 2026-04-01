f0350f111125fc0d89e93f847db47c8a85bdf1d9
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-04-01 18:00:21 +0530

    Time: 6 ms (91.11%), Space: 45.2 MB (70.72%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -16,3 +16,29 @@ class Solution {
         return sti.pop();
     }
 }
+/*
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> sti = new Stack<>();
+        for(String t:tokens){
+            if(isOperator(t)){
+                int b = sti.pop();
+                int a = sti.pop();
+                int res = result(a,b,t);
+                sti.push(res);
+            }
+            else sti.push(Integer.parseInt(t));
+        }
+        return sti.pop();
+    }
+    public boolean isOperator(String t){
+        return t.equals("+") || t.equals("-") || t.equals("*") || t.equals("/");
+    }
+    public int result(int a, int b, String op){
+        if(op.equals("+")) return a+b;
+        if(op.equals("-")) return a-b;
+        if(op.equals("*")) return a*b;
+        return a/b;
+    }
+}
+*/
