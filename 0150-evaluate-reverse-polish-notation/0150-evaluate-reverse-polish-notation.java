b906de11af09fac25bee6c12edff05861636b873
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-04-01 17:59:32 +0530

    Time: 6 ms (91.11%), Space: 45.2 MB (70.72%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -6,22 +6,12 @@ class Solution {
             if(s2.contains(token)){
                 int b = sti.pop();
                 int a = sti.pop();
-                if(token.equals("+")){
-                    sti.push(a+b);
-                }
-                else if(token.equals("-")){
-                    sti.push(a-b);
-                }
-                else if(token.equals("*")){
-                    sti.push(a*b);
-                }
-                else{
-                    sti.push(a/b);
-                }
+                if(token.equals("+")) sti.push(a+b);
+                else if(token.equals("-"))sti.push(a-b);
+                else if(token.equals("*")) sti.push(a*b);
+                else sti.push(a/b);
             }
-            else{
-                sti.push(Integer.parseInt(token));
-            }
+            else sti.push(Integer.parseInt(token));
         }   
         return sti.pop();
     }
