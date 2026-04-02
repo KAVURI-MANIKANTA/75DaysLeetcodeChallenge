1140d267514e9aa1f32a84ddbd78e1c6e375e6b3
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-04-02 19:15:03 +0530

    Time: 22 ms (96.05%), Space: 106.7 MB (70.62%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -14,16 +14,51 @@ class Solution {
     }
 }
 
+
+
+
 /*
-        int n = temperatures.length;
-        int[] res = new int[n];
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if(temperatures[i]<temperatures[j]){
-                    res[i] = j-i;
-                    break;
-                }
+Simple Comparison
+---------------------------------------------
+Feature	        Stack	    ArrayDeque
+---------------------------------------------
+Thread-safe	    ✅ Yes	    ❌ No
+Speed	        ❌ Slow	    ✅ Fast
+Recommended	    ❌ No	    ✅ Yes
+Internal DS	    Vector	      Dynamic Array
+---------------------------------------------
+*/
+
+
+
+/*
+//ArrayDeque beats 96%    tc:O(n), sc:O(n)
+// Stack beats 22%   tc:O(n), sc:O(n)
+
+    Stack<Integer> st = new Stack<>();
+    int n = temperatures.length;
+    int[] res = new int[n];
+    for(int i=n-1; i>=0; i--){
+        while(!st.isEmpty() && temperatures[i]>=temperatures[st.peek()]){
+           st.pop();
+        }
+        res[i] = st.isEmpty()?0:st.peek()-i;
+        st.push(i);
+    }
+    return res;
+*/
+
+
+/*  tc: O(n^2), sc: O(1)   result array is mentioned in the method so it is not considered
+    int n = temperatures.length;
+    int[] res = new int[n];
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(temperatures[i]<temperatures[j]){
+                res[i] = j-i;
+                break;
             }
         }
-        return res;
-        */
+    }
+    return res;
+*/
