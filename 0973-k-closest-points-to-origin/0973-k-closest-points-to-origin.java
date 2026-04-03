80c7a79559f894e6ce5be514d97c57a7f3854716
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-04-03 10:06:09 +0530

    Time: 22 ms (87.12%), Space: 55.1 MB (45.57%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,5 +1,31 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
+        int n = points.length;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->(a.dis-b.dis));
+        for(int i=0; i<n; i++){
+            int[] point = points[i];
+            int dis = point[0]*point[0]+point[1]*point[1];
+            pq.add(new Pair(dis,i));
+        }
+        int[][] res = new int[k][2];
+        for(int j=0; j<k; j++){
+            res[j] = points[pq.poll().idx];
+        }
+        return res;
+    }
+    class Pair{
+        int dis;
+        int idx;
+        Pair(int dis, int idx){
+            this.dis = dis;
+            this.idx = idx;
+        }
+    }
+}
+
+/*
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(dis(b)-dis(a)));
         for(int[] point:points){
             pq.offer(point);
@@ -18,3 +44,4 @@ class Solution {
         return point[0]*point[0]+point[1]*point[1];
     }
 }
+*/
