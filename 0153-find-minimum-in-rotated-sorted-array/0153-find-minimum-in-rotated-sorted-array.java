dec6f8199db2b8d184dc36c3d0cbcdced63575ed
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-04-06 13:58:10 +0530

    Time: 0 ms (100%), Space: 43.7 MB (63.87%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,6 +1,24 @@
 class Solution {
     public int findMin(int[] nums) {
+        int l=0;
+        int r=nums.length-1;
+        if(nums[l]<=nums[r]) return nums[l];
+        int mid = 0;
+        while(l<r){
+            mid = l+(r-l)/2;
+            if(nums[mid]<nums[r]) r=mid;
+            else l=mid+1;
+        }
+        return nums[l];
+    }
+}
+
+
+/*
+class Solution {
+    public int findMin(int[] nums) {
         Arrays.sort(nums);
         return nums[0];
     }
 }
+*/
