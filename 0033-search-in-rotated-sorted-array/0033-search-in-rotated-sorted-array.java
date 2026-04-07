c7ab53a27f5de8acc48af867c1c0129dae59268a
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-04-07 17:51:06 +0530

    Time: 0 ms (100%), Space: 43.8 MB (72.34%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,18 +1,5 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length-1;
-        for(int i=0; i<=n; i++){
-            if(nums[i]==target) return i;
-        }
-        return -1;
-    }
-}
-
-
-
-/*
-class Solution {
-    public int search(int[] nums, int target) {
         int l = 0;
         int r = nums.length-1;
         int mid = 0;
@@ -20,13 +7,25 @@ class Solution {
             mid = l+(r-l)/2;
             if(nums[mid]==target) return mid;
             if(nums[l]<=nums[mid]){
-                if(target>=nums[l] && target<=nums[mid]) r=mid;
+                if(target>=nums[l] && target<nums[mid]) r=mid-1;
                 else l=mid+1;
             }
             else{
                 if(target>nums[mid] && target<=nums[r]) l = mid+1;
-                else r=mid;
+                else r=mid-1;
             }
+        }
+        return -1;
+    }
+}
+
+
+/*
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length-1;
+        for(int i=0; i<=n; i++){
+            if(nums[i]==target) return i;
         }
         return -1;
     }
