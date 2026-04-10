7303df9e15d8299dbc3c2d26f6b2f6e8f582918a
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-04-10 19:23:15 +0530

    Time: 7 ms (9.47%), Space: 46.8 MB (40.03%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,6 +9,21 @@
  *     }
  * }
  */
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> hs = new HashSet<>();
+        ListNode temp = head;
+        while(temp!=null){
+            if(hs.contains(temp)) return true;
+            hs.add(temp);
+            temp = temp.next;
+        }
+        return false;
+    }
+}
+
+/*
 public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
@@ -22,3 +37,4 @@ public class Solution {
         return false;
     }
 }
+*/
