87587a3f878043dacf21653e5b8cc0fdc4173942
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-04-13 09:37:08 +0530

    Time: 0 ms (100%), Space: 43.2 MB (90.42%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,6 +10,23 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head;
+        for(int i=0; i<n; i++) fast = fast.next;
+        if(fast==null) return head.next;
+        ListNode slow = head;
+        while(fast.next!=null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
+
+
+/*
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode temp = head;
         int count = 0;
         while(temp!=null){
@@ -27,3 +44,4 @@ class Solution {
         return head;
     }
 }
+*/
