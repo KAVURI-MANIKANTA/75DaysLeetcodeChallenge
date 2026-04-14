a0b26d0c3618ea14f2f93578658943fe7e7822b2
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-04-14 13:08:31 +0530

    Time: 0 ms (100%), Space: 43.2 MB (15.1%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -13,6 +13,23 @@
  *     }
  * }
  */
+ class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null) return root;
+        Queue<TreeNode> QTree = new LinkedList<>();
+        QTree.add(root);
+        while(!QTree.isEmpty()){
+            TreeNode QRoot = QTree.poll();
+            TreeNode temp = QRoot.left;
+            QRoot.left = QRoot.right;
+            QRoot.right = temp;
+            if(QRoot.left!=null) QTree.add(QRoot.left);
+            if(QRoot.right!=null) QTree.add(QRoot.right);
+        }
+        return root;
+    }
+}
+/*
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if(root==null) return null;
@@ -23,5 +40,5 @@ class Solution {
         return root;
     }
 }
-
+*/
 
