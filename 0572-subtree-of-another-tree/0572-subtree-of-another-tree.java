3193a39d9525a50a87763738e4274f28556d740f
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-04-17 14:18:28 +0530

    Time: 2 ms (99.29%), Space: 46.3 MB (76.19%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -14,17 +14,14 @@
  * }
  */
 class Solution {
-    public String strtree(TreeNode node){
-        StringBuilder sb = new StringBuilder("^");
-        if(node==null) return "null";
-        sb.append(node.val);
-        sb.append(strtree(node.left));
-        sb.append(strtree(node.right));
-        return sb.toString();
+    public boolean isSame(TreeNode a, TreeNode b){
+        if(a==null && b==null) return true;
+        if(a==null || b==null) return false;
+        return a.val==b.val && isSame(a.left,b.left) && isSame(a.right,b.right);
     }
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        String fTree = strtree(root);
-        String subTree = strtree(subRoot);
-        return fTree.contains(subTree);
+        if(root==null) return false;
+        if(isSame(root,subRoot)) return true;
+        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
     }
 }
