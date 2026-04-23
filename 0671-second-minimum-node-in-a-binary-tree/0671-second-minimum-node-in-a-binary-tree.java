dcb68f96b6a864ec2b1bb164162dc48ace6ae3cf
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-04-23 10:26:05 +0530

    Time: 0 ms (100%), Space: 42.2 MB (98.24%) - LeetHub

--- 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.java
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.java
@@ -16,13 +16,13 @@
 class Solution {
     public int findSecondMinimumValue(TreeNode root) {
         if(root==null) return -1;
-        return dfs(root,root.val);
+        return dfsfind(root,root.val);
     }
-    public int dfs(TreeNode root, int min){
+    public int dfsfind(TreeNode root, int min){
         if(root==null) return -1;
         if(root.val>min) return root.val;
-        int left = dfs(root.left,min);
-        int right = dfs(root.right,min);
+        int left = dfsfind(root.left,min);
+        int right = dfsfind(root.right,min);
         if(left==-1) return right;
         if(right==-1) return left;
         return Math.min(left,right);
