f659c1887651c72aae59bbb3e67ee86339fa9c85
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-04-29 22:44:34 +0530

    Time: 13 ms (14.4%), Space: 52.5 MB (11.22%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,27 +1,21 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> set = new HashSet<>();
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                char num = board[i][j];
-
-                if (num == '.') continue;
-
-                String row = num + " in row " + i;
-                String col = num + " in col " + j;
-                String box = num + " in box " + (i/3) + "-" + (j/3);
-
-                if (set.contains(row) || set.contains(col) || set.contains(box)) {
+        HashSet<String> hs = new HashSet<>();
+        for(int i=0; i<9; i++){
+            for(int j = 0; j<9; j++){
+                char ch = board[i][j];
+                if(ch=='.') continue;
+                String row = i+"r"+ch;
+                String col = j+"c"+ch;
+                String box = (i/3)+""+(j/3)+ch;
+                if(hs.contains(row) || hs.contains(col) || hs.contains(box)){
                     return false;
                 }
-
-                set.add(row);
-                set.add(col);
-                set.add(box);
+                hs.add(row);
+                hs.add(col);
+                hs.add(box);
             }
         }
-
         return true;
     }
 }
