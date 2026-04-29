6758a06308c9a1f9d3cfde8849ad071c7c46daec
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-04-29 23:48:52 +0530

    Time: 2 ms (99%), Space: 47 MB (55.12%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.java
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.java
@@ -2,16 +2,13 @@ class Solution {
     public boolean checkXMatrix(int[][] grid) {
         int n = grid.length;
         for(int i=0; i<n; i++){
+            int sdi = n-i-1;
+            if(grid[i][i]==0 || grid[i][sdi]==0){
+                return false;
+            }
             for(int j=0; j<n; j++){
-                if(i==j || j==(n-i-1)){
-                    if(grid[i][j]==0){
-                        return false;
-                    }
-                }
-                else{
-                    if(grid[i][j]!=0){
-                        return false;
-                    }
+                if(j!=i && j!=sdi && grid[i][j]!=0){
+                    return false;
                 }
             }
         }
