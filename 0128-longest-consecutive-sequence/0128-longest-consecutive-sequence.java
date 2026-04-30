8ebedace7002d85e4c44e12c2c350d5efa6e44ad
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-04-30 13:04:04 +0530

    Time: 29 ms (82.07%), Space: 95.8 MB (25.73%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,5 +1,6 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
+        if(nums.length==0) return 0;
         HashSet<Integer> hs = new HashSet<>();
         for(int num:nums){
             hs.add(num);
@@ -13,7 +14,7 @@ class Solution {
                     nn++;
                     cl++;
                 }
-                ln = Math.max(ln,cl);
+                if(cl>ln) ln = cl;
             }
         }
         return ln;
