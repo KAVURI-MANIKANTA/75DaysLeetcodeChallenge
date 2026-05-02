509e73f70fae525ef16ef28b32a2b9612bd83407
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-05-02 09:26:11 +0530

    Time: 0 ms (100%), Space: 48.3 MB (37.59%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        char[] temp = new char[n];
+        for(int i=n-1; i>=0; i--){
+            temp[n-i-1] = s[i];
+        }
+        for(int i=0; i<n; i++){
+            s[i] = temp[i];
+        }
+    }
+}
