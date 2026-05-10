834dd145a5aa659f980f3c8a128650ac21c58458
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-05-10 21:55:46 +0530

    Time: 61 ms (77.21%), Space: 76.4 MB (86.38%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+
+    public int largestRectangleArea(int[] heights) {
+
+        int n = heights.length;
+
+        Stack<Integer> st = new Stack<>();
+
+        int maxArea = 0;
+
+        for(int i = 0; i <= n; i++){
+
+            while(!st.isEmpty() &&
+                  (i == n || heights[st.peek()] >= heights[i])){
+
+                int height = heights[st.pop()];
+
+                int nse = i;
+
+                int pse;
+
+                if(st.isEmpty()){
+                    pse = -1;
+                } else {
+                    pse = st.peek();
+                }
+
+                int width = nse - pse - 1;
+
+                int area = height * width;
+
+                maxArea = Math.max(maxArea, area);
+            }
+
+            st.push(i);
+        }
+
+        return maxArea;
+    }
+}
