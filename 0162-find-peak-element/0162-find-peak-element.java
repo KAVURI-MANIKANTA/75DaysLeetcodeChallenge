4a614ae8834f475fdc9cc637b67f015511c5b3ee
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-05-13 09:36:11 +0530

    Time: 0 ms (100%), Space: 44.4 MB (18.55%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -6,15 +6,15 @@ class Solution {
         int l=1;
         int r=n-2;
         while(l<=r){
-            int mid = l+(r-l)/2;
-            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]){
-                return mid;
+            int m = l+(r-l)/2;
+            if(nums[m-1]<nums[m] && nums[m]>nums[m+1]){
+                return m;
             }
-            else if(nums[mid-1]<nums[mid]){
-                l=mid+1;
+            else if(nums[m-1]<nums[m]){
+                l=m+1;
             }
             else{
-                r=mid-1;
+                r=m-1;
             }
         }
         return -1;
