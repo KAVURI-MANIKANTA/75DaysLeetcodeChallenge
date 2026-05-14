d17dfb13f8185d919f2b19bf0d2e24f260c214b0
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-05-14 22:13:24 +0530

    Time: 12 ms (54.52%), Space: 48 MB (41.5%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,30 @@
+class Solution {
+    public long midv(int[] piles,int num){
+        long s = 0;
+        for(int i=0; i<piles.length; i++){
+            s = s + (piles[i]/num);
+            if(piles[i]%num!=0){
+                s = s + 1;
+            }
+        }
+        return s;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1;
+        int r = 0;
+        for(int i=0; i<piles.length; i++){
+            r = Math.max(piles[i],r);
+        }
+        int mid = 0;
+        while(l<=r){
+            mid = l + (r-l)/2;
+            if(midv(piles,mid)>h){
+                l = mid + 1;
+            }
+            else{
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+}
