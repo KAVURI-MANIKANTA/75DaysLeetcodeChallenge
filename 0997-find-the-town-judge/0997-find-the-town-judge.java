47b127b07b54e9f8858354dc8469825ca8db5666
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-05-27 13:00:29 +0530

    Time: 3 ms (69.9%), Space: 53.5 MB (82.02%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,17 +1,12 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        HashMap<Integer,Integer> out = new HashMap<>();
-        HashMap<Integer,Integer> in = new HashMap<>();
+        int[] score = new int[n+1];
         for(int t[]:trust){
-            int a = t[0];
-            int b = t[1];
-            out.put(a,out.getOrDefault(a,0)+1);
-            in.put(b,in.getOrDefault(b,0)+1);
+            score[t[0]]--;
+            score[t[1]]++;
         }
         for(int i=1; i<=n; i++){
-            int outd = out.getOrDefault(i,0);
-            int ind = in.getOrDefault(i,0);
-            if(outd==0 && ind == n-1){
+            if(score[i]==(n-1)){
                 return i;
             }
         }
