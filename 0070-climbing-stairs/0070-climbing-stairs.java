9a13448c7498b08cd8b1a4120a56f42c0b388b39
KAVURI-MANIKANTA <manikantakavuri444@gmail.com>
2026-06-01 22:21:00 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,9 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n==0 || n==1 || n==2) return n;
-        int a=1;
-        int b=2;
-        int c=0;
+        if(n==1 || n==2) return n;
+        int a=1,b=2,c=0;
         for(int i=3; i<=n; i++){
-            c = a + b;
+            c=a+b;
             a=b;
             b=c;
         }
